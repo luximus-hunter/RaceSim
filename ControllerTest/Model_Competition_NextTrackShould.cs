6abc7273d1b6a8f8499870d0eed4fe0fbe99b0ba
luximus-hunter <thomaslipman10@gmail.com>
2022-09-13 13:20:50 +0200

    lvl3.1

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    public class Model_Competition_NextTrackShould
+    {
+        private Competition _competition;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _competition = new Competition();
+        }
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            Assert.IsNull(_competition.NextTrack());
+        }
+    }
+}
