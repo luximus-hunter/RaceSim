8a5a401021af94767733c29002f4e5ef6ce346e3
luximus-hunter <thomaslipman10@gmail.com>
2022-09-13 13:51:31 +0200

    lvl3.4

--- ControllerTest/Model_Competition_NextTrackShould.cs
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -21,7 +21,54 @@ namespace ControllerTest
         [Test]
         public void NextTrack_EmptyQueue_ReturnNull()
         {
-            Assert.IsNull(_competition.NextTrack());
+            Track result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void NextTrack_OneInQueue_ReturnTrack()
+        {
+            SectionTypes[] t = { SectionTypes.StartGrid, SectionTypes.Finish, SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.Straight, SectionTypes.Straight, SectionTypes.RightCorner, SectionTypes.RightCorner };
+            Track track = new Track("Track 1", t);
+
+            _competition.Tracks.Enqueue(track);
+
+            Track result = _competition.NextTrack();
+
+            Assert.AreEqual(track, result);
+        }
+
+        [Test]
+        public void NextTrack_OneInQueue_RemoveTrackFromQueue()
+        {
+            SectionTypes[] t = { SectionTypes.StartGrid, SectionTypes.Finish, SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.Straight, SectionTypes.Straight, SectionTypes.RightCorner, SectionTypes.RightCorner };
+            Track track = new Track("Track 1", t);
+
+            _competition.Tracks.Enqueue(track);
+
+            Track result = _competition.NextTrack();
+            result = _competition.NextTrack();
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void NextTrack_TwoInQueue_ReturnNextTrack()
+        {
+            SectionTypes[] t = { SectionTypes.StartGrid, SectionTypes.Finish, SectionTypes.RightCorner, SectionTypes.RightCorner, SectionTypes.Straight, SectionTypes.Straight, SectionTypes.RightCorner, SectionTypes.RightCorner };
+            Track track1 = new Track("Track 1", t);
+            Track track2 = new Track("Track 2", t);
+
+            _competition.Tracks.Enqueue(track1);
+            _competition.Tracks.Enqueue(track2);
+
+            Track result = _competition.NextTrack();
+
+            Assert.AreEqual(result.Name, track1.Name);
+
+            result = _competition.NextTrack();
+
+            Assert.AreEqual(result.Name, track2.Name);
         }
     }
 }
